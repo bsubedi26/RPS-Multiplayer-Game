5e2d58eb1b6d64f1ad071951bdaf3d4de829180b
Bibek Subedi <bsubedi26@gmail.com>
2016-05-12 14:28:44 -0400

    Several css fixes and added comments for the javascript logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
-        // Sets the computer choices 
+        // Assign the firebase URL database to new variable 
         var firebase = new Firebase("https://rpsmultiplayer.firebaseio.com/");
-        // Declares the tallies to 0 
+        // Declare global variables 
         var player1;
         var player2;
         var player1Wins = 0;
@@ -11,20 +11,23 @@ $(document).ready(function() {
         var player2Losses = 0;
         var player2choice;
         var ties = 0;
+        //Adding the initial name form and button selections to the page
         $("#top").html("<input id='name' placeholder='NAME'>"+"<hr>"+"<button id='start'>Start</button>")
         $("#player1").html("<button id='rock'>Rock</button>"+"<br>"+ "<button id='paper'>Paper</button>"+"<br>"+ "<button id='scissors'>Scissors</button>")
         $("#player2").html("<button id='rock2'>Rock</button>"+"<br>"+ "<button id='paper2'>Paper</button>"+"<br>"+ "<button id='scissors2'>Scissors</button>")
 
-        //Get player name
+        //Button that gets the player name on click
         $("#start").on("click", function() { 
             var name = $("#name").val();
             console.log(name);
         })
+        //On click function that gets the player 1 choice based on which button is clicked & executes the check function
         $("#player1 button").on("click", function() {
             player1choice = $(this).text();
             console.log(player1choice);
             check();
         })
+        //On click function that gets the player 2 choice based on which button is clicked & executes the check function
         $("#player2 button").on("click", function() {
             player2choice = $(this).text();
             console.log(player2choice);
@@ -32,7 +35,7 @@ $(document).ready(function() {
            
         })
         
-        //Check player choices and assign winner/loser
+        //Rock paper scissors logic that checks both player choices, assigns winner/loser & executes the firebaseSet function to send the results to firebase
         function check() {
             if (player1choice == 'Rock' && player2choice =='Scissors') {
                 player1Wins++;
@@ -78,6 +81,7 @@ $(document).ready(function() {
         //check function closer
         }
 
+        //Updates firebase database in real time
         function firebaseSet() {
         firebase.set({
             player1: name,
