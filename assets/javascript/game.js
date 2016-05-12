c8515aea597c41ed51df0288db7717d8cb84f9eb
Bibek Subedi <bsubedi26@gmail.com>
2016-05-12 13:46:09 -0400

    added initial firebase logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
         // Sets the computer choices 
-        var computerChoices = ['r', 'p', 's'];
+        var firebase = new Firebase("https://rpsmultiplayer.firebaseio.com/");
         // Declares the tallies to 0 
         var player1;
         var player2;
@@ -15,6 +15,11 @@ $(document).ready(function() {
         $("#player1").html("<button id='rock'>Rock</button>"+"<br>"+ "<button id='paper'>Paper</button>"+"<br>"+ "<button id='scissors'>Scissors</button>")
         $("#player2").html("<button id='rock2'>Rock</button>"+"<br>"+ "<button id='paper2'>Paper</button>"+"<br>"+ "<button id='scissors2'>Scissors</button>")
 
+        //Get player name
+        $("#start").on("click", function() { 
+            var name = $("#name").val();
+            console.log(name)
+        })
         $("#player1 button").on("click", function() {
             player1choice = $(this).text();
             console.log(player1choice)
@@ -27,6 +32,18 @@ $(document).ready(function() {
             player2choice = $(this).text();
             console.log(player2choice)
            
+        })
+        
+
+        firebase.set({
+            player1: name,
+            // player1choice: player1choice,
+            player1Wins: player1Wins,
+            player1Losses: player1Losses,
+            // player2choice: player2choice,
+            player2: name,
+            player2Wins: player2Wins,
+            player2Losses: player2Losses
         })
         // if ((player1choice == 'r') || (userGuess == 'p') || (userGuess == 's')){
         // // It tests to determine if the computer or the user won the round and then increments 
